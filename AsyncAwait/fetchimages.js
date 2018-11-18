4f570b34ecc2520a82f7245bff36ae375289dcb4
Daiwei Chen <kinbd8@gmail.com>
2018-11-18 16:46:26 -0600

    Fixing merge problems with dropbox de-sync

--- AsyncAwait/fetchimages.js
+++ AsyncAwait/fetchimages.js
@@ -3,48 +3,49 @@ const request = require('request');
 const file = require('fs');
 
 async function fetchImageUrls(url, imgNum){
-    let imgList = [];
-    const res = await fetch(url);
-    const data = await res.json();
+  let imgList = [];
+  const res = await fetch(url);
+  const data = await res.json();
 
-    let imgCount = 0;
-    for(let imgData of data){
-        imgList.push(imgData['url']);
-        imgCount++;
-        if(imgCount >= imgNum){
-            break;
-        }
+  let imgCount = 0;
+  for(let imgData of data){
+    imgList.push(imgData['url']);
+    imgCount++;
+    if(imgCount >= imgNum){
+      break;
     }
+  }
 
-    return imgList;
+  return imgList;
 }
 
-async function downloadImg(url, imgName, ){
-    request.head(url, function(err, res, body){
-      console.log('Downloading '+imgName+'...');
+async function downloadImg(url, imgName){
+  console.log('Downloading '+imgName+'...');
 
-        request(url).pipe(file.createWriteStream('./Images/'+imgName).on('close', function(){
-          console.log('Downloaded '+imgName+'!');
-        }));
-    });
+
+  request.head(url, function(err, res, body){
+    request(url).pipe(file.createWriteStream('./Images/'+imgName).on('close', function(){
+      console.log('Downloaded '+imgName+'!');
+    }));
+  });
 }
 
 async function appendImgs(url, imgNum){
-    imgList = await fetchImageUrls(url, imgNum);
-    console.log(imgList);
+  imgList = await fetchImageUrls(url, imgNum);
+  console.log(imgList);
 
-    let imgCount = 0;
-    for(let imgUrl of imgList){
-      downloadImg(imgUrl, 'image'+imgCount+'.png');
-        imgCount++;
-        if(imgCount > imgNum){
-            break;
-        }
+  let imgCount = 0;
+  for(let imgUrl of imgList){
+    downloadImg(imgUrl, 'image'+imgCount+'.png');
+    imgCount++;
+    if(imgCount > imgNum){
+      break;
     }
+  }
 }
 
 let photosUrl = 'https://jsonplaceholder.typicode.com/photos';
 
-appendImgs(photosUrl, 1000);
+appendImgs(photosUrl, 50);
 
 console.log(fetch('https://via.placeholder.com/600/92c952'));
